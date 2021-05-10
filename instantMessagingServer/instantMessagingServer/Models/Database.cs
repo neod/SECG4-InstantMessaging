ecbbef089b5679fcbd0fefa9dd7fcc6ee6c51bb4
Damiano Deplano <54456@etu.he2b.be>
2021-05-10 21:22:07 +0200

    add server project with Users class and sqlite connexion

---
+++ instantMessagingServer/instantMessagingServer/Models/Database.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace instantMessagingServer.Models
+{
+    public class Database : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=instantMessaging.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().HasKey(u => u.Id);
+        }
+    }
+}
