6d88def819f29383f3111fb93f98e1ed03355d78
Damiano Deplano <54456@etu.he2b.be>
2021-05-14 16:25:36 +0200

    add a library project to share with client and server, and add a RSA manager class

---
+++ instantMessagingServer/Crypto/RSAManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypto
+{
+    public class RSAManager
+    {
+        private static RSAManager CryptoInstance;
+        private readonly RSAEncryptionPadding EncryptionPadding = RSAEncryptionPadding.Pkcs1;
+        private readonly RSASignaturePadding SignaturPadding = RSASignaturePadding.Pkcs1;
+        private readonly HashAlgorithmName hashAlgorithmName = HashAlgorithmName.SHA512;
+
+        public static RSAManager getInstance()
+        {
+            return CryptoInstance ??= new RSAManager();
+        }
+
+        private RSA Rsa { get; set; }
+
+        public void SetKey(string key)
+        {
+            Rsa.FromXmlString(key);
+        }
+
+        public string GetKey(bool withPrivateKey)
+        {
+            return Rsa.ToXmlString(withPrivateKey);
+        }
+
+        private RSAManager()
+        {
+            Rsa = RSA.Create();
+        }
+
+        #region Encrypt
+
+        public byte[] Encrypt(byte[] data)
+        {
+            return Rsa.Encrypt(data, EncryptionPadding);
+        }
+
+        public byte[] Encrypt(string data)
+        {
+            return Encrypt(Encoding.UTF8.GetBytes(data));
+        }
+
+        public string EncryptToBase64(byte[] data)
+        {
+            return Convert.ToBase64String(Encrypt(data));
+        }
+
+        public string EncryptToBase64(string data)
+        {
+            return Convert.ToBase64String(Encrypt(data));
+        }
+
+        #endregion Encrypt
+
+        #region Decrypt
+
+        public byte[] Decrypt(byte[] data)
+        {
+            return Rsa.Decrypt(data, EncryptionPadding);
+        }
+        public byte[] DecryptFromBase64(string data)
+        {
+            return Decrypt(Convert.FromBase64String(data));
+        }
+
+        public string DecryptToString(byte[] data)
+        {
+            return Encoding.UTF8.GetString(Decrypt(data));
+        }
+        public string DecryptFromBase64ToString(string data)
+        {
+            return Encoding.UTF8.GetString(DecryptFromBase64(data));
+        }
+
+        #endregion Decrypt
+
+        #region Sign
+
+        public byte[] Sign(byte[] data)
+        {
+            return Rsa.SignData(data, hashAlgorithmName, SignaturPadding);
+        }
+
+        public byte[] Sign(string data)
+        {
+            return Sign(Encoding.UTF8.GetBytes(data));
+        }
+
+        public string SignToBase64(byte[] data)
+        {
+            return Convert.ToBase64String(Sign(data));
+        }
+
+        public string SignToBase64(string data)
+        {
+            return Convert.ToBase64String(Sign(Encoding.UTF8.GetBytes(data)));
+        }
+
+        #endregion Sign
+
+    }
+}
