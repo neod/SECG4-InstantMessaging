785f27ee539bf47fa74138b55ca973ca91d0b849
Damiano Deplano <54456@etu.he2b.be>
2021-05-18 22:11:55 +0200

    simplifie the RSAManager

--- instantMessagingServer/Crypto/RSAManager.cs
+++ instantMessagingServer/Crypto/RSAManager.cs
@@ -12,86 +12,74 @@ namespace Crypto
 
         private RSA Rsa { get; set; }
 
+        /// <summary>
+        /// Return the current key(s) in xml format
+        /// </summary>
+        /// <param name="withPrivateKey">include private key</param>
+        /// <returns>The key(s) in xml format</returns>
         public string GetKey(bool withPrivateKey)
         {
             return Rsa.ToXmlString(withPrivateKey);
         }
 
+        /// <summary>
+        /// Instance a RSA with a new keys
+        /// </summary>
+        public RSAManager()
+        {
+            Rsa = RSA.Create();
+        }
+
+        /// <summary>
+        /// Instance a RSA with this key
+        /// </summary>
+        /// <param name="key">The xml fromated key to use in RSA</param>
         public RSAManager(string key)
         {
             Rsa = RSA.Create();
             Rsa.FromXmlString(key);
         }
 
-        #region Encrypt
-
+        /// <summary>
+        /// Encrypt a data byte array
+        /// </summary>
+        /// <param name="data">The byte array of data to encrypt</param>
+        /// <returns>A byte array of data encrypted</returns>
         public byte[] Encrypt(byte[] data)
         {
             return Rsa.Encrypt(data, EncryptionPadding);
         }
 
-        public byte[] Encrypt(string data)
-        {
-            return Encrypt(Encoding.UTF8.GetBytes(data));
-        }
-
-        public string EncryptToBase64(byte[] data)
-        {
-            return Convert.ToBase64String(Encrypt(data));
-        }
-
-        public string EncryptToBase64(string data)
-        {
-            return Convert.ToBase64String(Encrypt(data));
-        }
-
-        #endregion Encrypt
-
-        #region Decrypt
-
+        /// <summary>
+        /// Decrypt a data byte array
+        /// </summary>
+        /// <param name="data">The byte array of data to decrypt</param>
+        /// <returns>A byte array of data decrypted</returns>
         public byte[] Decrypt(byte[] data)
         {
             return Rsa.Decrypt(data, EncryptionPadding);
         }
-        public byte[] DecryptFromBase64(string data)
-        {
-            return Decrypt(Convert.FromBase64String(data));
-        }
 
-        public string DecryptToString(byte[] data)
-        {
-            return Encoding.UTF8.GetString(Decrypt(data));
-        }
-        public string DecryptFromBase64ToString(string data)
-        {
-            return Encoding.UTF8.GetString(DecryptFromBase64(data));
-        }
-
-        #endregion Decrypt
-
-        #region Sign
-
+        /// <summary>
+        /// Sign the data with the private key
+        /// </summary>
+        /// <param name="data">The data to sign</param>
+        /// <returns>The signed data</returns>
         public byte[] Sign(byte[] data)
         {
             return Rsa.SignData(data, hashAlgorithmName, SignaturPadding);
         }
 
-        public byte[] Sign(string data)
+        /// <summary>
+        /// Verify the signed data with the public key
+        /// </summary>
+        /// <param name="data">the data to sign</param>
+        /// <param name="signature">The signature to use</param>
+        /// <returns>True if the signature is correct</returns>
+        public bool Verify(byte[] data, byte[] signature)
         {
-            return Sign(Encoding.UTF8.GetBytes(data));
+            return Rsa.VerifyData(data, signature, hashAlgorithmName, SignaturPadding);
         }
-
-        public string SignToBase64(byte[] data)
-        {
-            return Convert.ToBase64String(Sign(data));
-        }
-
-        public string SignToBase64(string data)
-        {
-            return Convert.ToBase64String(Sign(Encoding.UTF8.GetBytes(data)));
-        }
-
-        #endregion Sign
 
     }
 }
