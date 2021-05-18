d565c70036220b5d8715490bb656dcf29a08d6f6
Damiano Deplano <54456@etu.he2b.be>
2021-05-18 22:53:23 +0200

    Add empty instantMessagingClient solution

---
+++ instantMessagingClient/instantMessagingClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace instantMessagingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello instant messaging World!");
+        }
+    }
+}
