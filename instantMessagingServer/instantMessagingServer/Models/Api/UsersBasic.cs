2b49919074ffe796007ed9a4c563629bdd923ca9
Damiano Deplano <54456@etu.he2b.be>
2021-05-18 22:17:36 +0200

    Move api entity to Api directory

--- instantMessagingServer/instantMessagingServer/Models/Api/UsersBasic.cs
+++ instantMessagingServer/instantMessagingServer/Models/Api/UsersBasic.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace instantMessagingServer.Models
+namespace instantMessagingServer.Models.Api
 {
     public class UsersBasic
     {
