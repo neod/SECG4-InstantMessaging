b94f386b92dec6d8a8bac38648530eba37220d33
onra2 <arno.pion007@gmail.com>
2021-05-20 11:54:30 +0200

    Basic menu

--- instantMessagingClient/instantMessagingClient/Program.cs
+++ instantMessagingClient/instantMessagingClient/Program.cs
@@ -1,12 +1,148 @@
 ﻿using System;
+using System.Security;
+using System.Threading.Tasks;
+using EasyConsoleApplication;
+using EasyConsoleApplication.Menus;
+using EasyConsoleApplication.Pages;
 
 namespace instantMessagingClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static SecureString getPasswordFromConsole(string displayMessage)
         {
-            Console.WriteLine("Hello instant messaging World!");
+            SecureString pass = new SecureString();
+            Console.Write(displayMessage);
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true);
+
+                if (!char.IsControl(key.KeyChar))
+                {
+                    pass.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key != ConsoleKey.Backspace || pass.Length <= 0) continue;
+                    pass.RemoveAt(pass.Length - 1);
+                    Console.Write("\b \b");
+                }
+            }
+            while (key.Key != ConsoleKey.Enter);
+            return pass;
+        }
+
+        private static void Main(string[] args)
+        {
+            // set some global settings
+            ConsoleSettings.DefaultColor = ConsoleColor.White;
+
+            // define the menu
+            Menu mainMenu = new Menu("Instant messaging client");
+            Application.GoTo<HomePage>();
+
+            Application.Render(mainMenu);
+            Console.WriteLine("Application Terminated.");
+            ConsoleHelpers.HitEnterToContinue();
+
+
+            //string[] menuItems = { "Login", "Inscription"};
+            //Menu menuMain = new Menu(menuItems, "Main Menu");
+
+            /*switch (menuMain.displayMenu())
+            {
+                case 0:
+                {
+                    Console.WriteLine("Veuillez-vous connecter.");
+                    Console.WriteLine("------------------------");
+                    Console.Write("User: ");
+                    string user = Console.ReadLine();
+                    SecureString password = getPasswordFromConsole("Password: ");
+                    Console.WriteLine();
+                    Console.WriteLine("\nBonjour " + user + "\n");
+                }
+                    break;
+                case 1:
+                {
+                    Console.WriteLine("Choissisez un nom d'utilisateur et un mot de passe.");
+                    Console.WriteLine("---------------------------------------------------");
+                    Console.Write("User: ");
+                    string user = Console.ReadLine();
+                    SecureString password = getPasswordFromConsole("Password: ");
+                    Console.WriteLine();
+                    Console.WriteLine("\nBonjour " + user + "\n");
+                }
+                    break;
+                default:
+                    Console.WriteLine("whu?");
+                    break;
+            }*/
+        }
+
+        public class HomePage : Page
+        {
+            public HomePage()
+            {
+                Title = "Home";
+                TitleColor = ConsoleColor.Green;
+                Body = "----";
+                BodyColor = ConsoleColor.DarkGreen;
+                MenuItems.Add(new MenuItem("Login", () => Application.GoTo<LoginPage>()));
+                MenuItems.Add(new MenuItem("Register", () => Application.GoTo<RegisterPage>())
+                {
+                    Color = ConsoleColor.Yellow
+                });
+                MenuItems.Add(Separator.Instance);
+                MenuItems.Add(new MenuItem("Quit", Application.Exit));
+            }
+        }
+
+        public class LoginPage : Page
+        {
+            public LoginPage()
+            {
+                Title = "Login";
+                Body = "-----";
+                
+            }
+        }
+
+        public class RegisterPage : Page
+        {
+            public RegisterPage()
+            {
+                Title = "Register";
+                TitleColor = ConsoleColor.Yellow;
+                Body = "-----";
+                string user = ConsoleHelpers.Readline(ConsoleColor.White, "Username: ");
+                SecureString password = getPasswordFromConsole("Password: ");
+                Console.WriteLine();
+                ConsoleHelpers.Write(ConsoleColor.White, "Successfully registered " + user);
+                ConsoleHelpers.HitEnterToContinue();
+                Application.GoTo<LoginPage>();
+            }
+        }
+
+        public class PageAvecParametre : Page
+        {
+            private readonly string _dependency;
+
+            public PageAvecParametre(string dependency)
+            {
+                _dependency = dependency;
+                Title = "blabla";
+                TitleColor = ConsoleColor.Yellow;
+                Body = "-----";
+                MenuItems.Add(new MenuItem("Back", Application.GoBack));
+                MenuItems.Add(new MenuItem("Option 1", () => Console.WriteLine($"{_dependency} Action 2")));
+                MenuItems.Add(new MenuItem("Option 2", () => Console.WriteLine($"{_dependency} Action 3")));
+                MenuItems.Add(Separator.Instance);
+                MenuItems.Add(new MenuItem("Back", Application.GoBack));
+                MenuItems.Add(new MenuItem("Quit", Application.Exit));
+            }
         }
     }
 }
