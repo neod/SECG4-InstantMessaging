6c9a750a2714f20d284d4911bcb869e1558df590
onra2 <arno.pion007@gmail.com>
2021-05-25 12:57:50 +0200

    whoopsie

--- instantMessagingServer/instantMessagingServer/Controllers/FriendsController.cs
+++ instantMessagingServer/instantMessagingServer/Controllers/FriendsController.cs
@@ -134,7 +134,7 @@ namespace instantMessagingServer.Controllers
             IActionResult response = Unauthorized();
 
             var ClaimIDToken = User.Claims.FirstOrDefault((c) => c.Type == "IDToken");
-            if (ClaimIDToken != null && authentication.isAutheticate(User.Identity?.Name, ClaimIDToken))
+            if (ClaimIDToken != null && authentication.isAutheticate(User.Identity.Name, ClaimIDToken))
             {
                 DatabaseContext db = new(Configuration);
 
@@ -162,5 +162,6 @@ namespace instantMessagingServer.Controllers
 
             return response;
         }
+
     }
 }
